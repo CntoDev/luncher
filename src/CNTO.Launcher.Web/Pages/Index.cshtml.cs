8018f733d16ff39acafd2fbfd7f474aafe5aff2e
Highway <milanimilivoj@gmail.com>
2022-03-08 20:16:31 +0100

    Add Western Sahara

--- src/CNTO.Launcher.Web/Pages/Index.cshtml.cs
+++ src/CNTO.Launcher.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using CNTO.Launcher.Application;
 using CNTO.Launcher.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CNTO.Launcher.Web.Pages;
@@ -22,7 +23,11 @@ public class IndexModel : PageModel
         _launcherService = launcherService;
         _repositoryCollection = repositoryCollection;
         _logger = logger;
+        Dlcs = new DlcCheckboxes();
     }
+
+    [BindProperty]
+    public DlcCheckboxes Dlcs { get; set; }
 
     public void OnGet()
     {
@@ -41,7 +46,18 @@ public class IndexModel : PageModel
         List<RepositoryId> repositoryIds = new();
         repositoryIds.Add(new("Main"));
         repositoryIds.Add(new("Server Only"));
-        await _launcherService.StartServerAsync(repositoryIds, new List<Dlc>(), 2);
+        List<Dlc> startingDlcs = BuildDlcList();
+        await _launcherService.StartServerAsync(repositoryIds, startingDlcs, 2);
+    }
+
+    private List<Dlc> BuildDlcList()
+    {
+        List<Dlc> startingDlcs = new();
+        if (Dlcs.Vn) startingDlcs.Add(new("vn"));
+        if (Dlcs.Gm) startingDlcs.Add(new("gm"));
+        if (Dlcs.Csla) startingDlcs.Add(new("csla"));
+        if (Dlcs.Ws) startingDlcs.Add(new("ws"));
+        return startingDlcs;
     }
 
     public async Task OnPostStartCampaignAsync()
@@ -51,6 +67,15 @@ public class IndexModel : PageModel
         repositoryIds.Add(new("Main"));
         repositoryIds.Add(new("Campaign"));
         repositoryIds.Add(new("Server Only"));
-        await _launcherService.StartServerAsync(repositoryIds, new List<Dlc>(), 2);
-    }    
+        List<Dlc> startingDlcs = BuildDlcList();
+        await _launcherService.StartServerAsync(repositoryIds, startingDlcs, 2);
+    }
+
+    public class DlcCheckboxes
+    {
+        public bool Vn { get; set; }
+        public bool Gm { get; set; }
+        public bool Csla { get; set; }
+        public bool Ws { get; set; }
+    }
 }
