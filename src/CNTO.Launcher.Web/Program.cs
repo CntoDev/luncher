9cfc5968125ec38fff277b4a23ad706d6a4c1c1f
Highway <milanimilivoj@gmail.com>
2022-05-15 07:19:15 +0200

    Deploy as windows service

--- src/CNTO.Launcher.Web/Program.cs
+++ src/CNTO.Launcher.Web/Program.cs
@@ -16,6 +16,7 @@ Log.Logger = new LoggerConfiguration()
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
+builder.Host.UseWindowsService();
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
